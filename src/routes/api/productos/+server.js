a90ef2e8098e02e78477b4121812fa5d5be198db
james <jbolanosdiaz@gmail.com>
2024-12-07 17:41:57 -0600

    change the library from using pool to sql

--- src/routes/api/productos/+server.js
+++ src/routes/api/productos/+server.js
@@ -1,10 +1,20 @@
-import { pool } from '@vercel/postgres';
+import { sql } from '@vercel/postgres';
 
-export async function GET({ query }) {
-    const { barcode } = query;
-    const { rows } = await pool.query('SELECT * FROM productos WHERE codigo_barras = $1', [barcode]);
-    return {
-        body: rows[0] || {}  // Return the product details if found
-    };
+export async function GET({ url }) {
+    const barcode = url.searchParams.get('barcode');
+
+    try {
+        const rows = await sql`SELECT * FROM productos WHERE codigo_barras = ${barcode}`;
+
+        return {
+            status: 200,
+            body: rows[0] || {}, // Return the product details if found
+        };
+    } catch (error) {
+        console.error('Database query error:', error);
+        return {
+            status: 500,
+            body: { error: 'Internal server error' },
+        };
+    }
 }
-
